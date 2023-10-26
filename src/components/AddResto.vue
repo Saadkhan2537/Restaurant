f608ad18e6f79ac1c7f4deebf3d409b5b2d208a3
unknown <saad mehboob>
2023-10-26 15:59:21 +0500

    updated

--- src/components/AddResto.vue
+++ src/components/AddResto.vue
@@ -4,15 +4,16 @@
 <form class="register">
     <h2>Add Resaurant</h2>
     <input type="text" name="name" placeholder="Enter Name" v-model="restaurant.name">
-    <input type="address" name="address" placeholder="Enter Address" v-model="restaurant.address">
-    <input type="contatc" name="contatc" placeholder="Enter Contatc No" v-model="restaurant.contact">
-    <button v-on:click="AddRestaurant()">Add New Restaurant</button>
+    <input type="text" name="address" placeholder="Enter Address" v-model="restaurant.address">
+    <input type="text" name="contatc" placeholder="Enter Contatc No" v-model="restaurant.contact">
+    <button v-on:click="addRestaurant">Add New Restaurant</button>
 </form>
 </template>
 
 <script>
 import HeaderView from './HeaderView.vue'
 import axios from 'axios';
+
 export default {
     name: 'AddResto',
     components: {
@@ -27,27 +28,31 @@ export default {
             }
         }
     },
-   async AddRestaurant() {
-        //console.warn(this.restaurant)
-        const result=await axios.post("http://localhost:3000/restaurant",
-        {
-            name:this.restaurant.name,
-            contact:this.restaurant.contact,
-            address:this.restaurant.address,
-        });
-        console.warn('Result',result);
-    }, 
+    methods: {
+        async addRestaurant() {
+            try {
+                const result = await axios.post("http://localhost:3000/restaurant", {
+                    name: this.restaurant.name,
+                    contact: this.restaurant.contact,
+                    address: this.restaurant.address,
+                });
+                console.log('Result', result);
+            } catch (error) {
+                console.error('Error adding restaurant:', error);
+            }
+        },
+    },
     mounted() {
         let users = localStorage.getItem('user-info');
         if (!users) {
             this.$router.push({
                 name: 'SignUp'
-            })
+            });
         }
-
     }
 }
 </script>
+
 
 <style>
 
